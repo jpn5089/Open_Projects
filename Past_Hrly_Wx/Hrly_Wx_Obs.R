03af6c69c0ef9973ec2b3a2118ef9875e5bfd6aa
jpn5089 <jp19nicola@gmail.com>
2016-03-20 16:19:11 -0400

    Updates

--- Past_Hrly_Wx/Hrly_Wx_Obs.R
+++ Past_Hrly_Wx/Hrly_Wx_Obs.R
@@ -3,6 +3,8 @@ library(spdep)
 library(sp)
 library(fields)
 library(MBA)
+library(dplyr)
+library(R.utils)
 
 # reference doc: http://blue.for.msu.edu/lab-notes/NOAA_0.1-1/NOAA-ws-data.pdf
 
@@ -17,9 +19,9 @@ st <- read.csv("~/GitHub/Open_Projects/Past_Hrly_Wx/Data/isd-history.csv")
 points <- read.csv("~/GitHub/EnergyPriceForecastComp/Points.csv")
 
 #st <- st[st$CTRY %in% c("SP", "PO"),]
-for (p in 1:18) {
-  st <- st[st$USAF %in% (points[4,p])]
-}
+#for (p in 1:18) {
+#  st <- st[st$USAF %in% (points[4,p])]
+#}
 #st <- st[st$USAF == points[p,4]]
 #st$LAT <- st$LAT/1000
 #st$LON <- st$LON/1000
@@ -27,27 +29,29 @@ for (p in 1:18) {
 st$BEGIN <- as.numeric(substr(st$BEGIN, 1, 4))
 st$END <- as.numeric(substr(st$END, 1, 4))
 
+x <- points[[4]]
 
-sp.list <- st[st$USAF %in% c("80020", "85670") & (st$BEGIN <= 2014 & st$END >= 2016 & !is.na(st$BEGIN)), ]
-dim(pa.list)
+points.list <- st[st$USAF %in% x,]
 
-outputs <- as.data.frame(matrix(NA, dim(sp.list)[1],2))
+points.list <- left_join(points.list,points) %>%
+  select(point, USAF, WBAN, STATION.NAME, CTRY, ELEV.M., BEGIN, END)
+
+#& (st$BEGIN <= 2014 & st$END >= 2016 & !is.na(st$BEGIN)), ]
+#dim(points.list)
+
+outputs <- as.data.frame(matrix(NA, dim(points.list)[1],2))
 
 names(outputs) <- c("Point","FILE")
 for (y in 2014:2016) {
-  y.sp.list <- sp.list[sp.list$BEGIN <= y & sp.list$END >= y, ]
-  for (s in 1:dim(y.sp.list)[1]) {
-    outputs[s, 2] <- paste(sprintf("%06d", y.sp.list[s,
-        1]), "-", sprintf("%05d", y.sp.list[s,
-        2]), "-", y, ".gz", sep = "")
+  y.points.list <- points.list[points.list$BEGIN <= y & points.list$END >= y, ]
+  for (s in 1:dim(y.points.list)[1]) {
+    outputs[s, 2] <- paste(sprintf("%06d", y.points.list[s,
+        2]), "-", sprintf("%05d", y.points.list[s,
+        3]), "-", y, ".gz", sep = "")
     try(download.file(paste("ftp://ftp.ncdc.noaa.gov/pub/data/noaa/", y, "/", outputs[s,2], sep = ""),
-                  paste("~/GitHub/Open_Projects/Past_Hrly_Wx/Data/", outputs[s,2], sep = ""), method = "libcurl"))
+                  paste("~/GitHub/Open_Projects/Past_Hrly_Wx/Data/ES_PT_Wx/", points.list[s,1], "-", outputs[s,2], sep = ""), method = "libcurl"))
   }
 }
-head(pa.list)
-head(outputs)
 
-dir.create(paste(getwd(),"/GitHub/Open_Projects/Past_Hrly_Wx/Data/ES_PT",sep = ""))
-untar(paste(getwd(), "/GitHub/Open_Projects/Past_Hrly_Wx/Data/",))
-
-g <- gunzip("~/GitHub/Open_Projects/Past_Hrly_Wx/Data/080020-99999-2014.gz")
+g <- gunzip("~/GitHub/Open_Projects/Past_Hrly_Wx/Data/ES_PT_Wx/1-080020-99999-2015.gz")
+data <- read.table(gzfile(paste(getwd(), "~/GitHub/Open_Projects/Past_Hrly_Wx/Data/ES_PT_Wx/1-080020-99999-2015", sep = ""), open = "rt"), sep = "", header = FALSE, skip =1, colClasses = classes)
